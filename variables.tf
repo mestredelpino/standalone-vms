650b56cfacb302c7b448fd1f94135df46f85b5af
carlosmestredp <carlosmestredp@gmail.com>
2022-04-12 14:32:15 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -61,10 +61,12 @@ variable "focal-cloudserver-name" {
 
 variable "dhcp-vms" {
   description = "The virtual machines to be deployed with a dynamic IP address"
+  type = map
   default = []
 }
 
 variable "static-vms" {
   description  = "The virtual machines to be deployed with a static IP address"
+  type = map
   default      = []
 }
